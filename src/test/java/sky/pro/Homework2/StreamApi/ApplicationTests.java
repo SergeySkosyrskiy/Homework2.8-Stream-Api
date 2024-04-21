b946465fd1d14b3b156f0c1122650260a0833617
SergeySkosyrskiy <s-s-e_96@mail.ru>
2024-04-21 10:27:52 +0800

    HW2.8

---
+++ src/test/java/sky/pro/Homework2/StreamApi/ApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.Homework2.StreamApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
